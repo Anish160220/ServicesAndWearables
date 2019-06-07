5f80cd530e72f7e771e4eef252a96031c1acb4e6
Softwarica College Lab <softwarica@softwarica.com>
2019-06-07 07:51:21 +0545

    Channel Created

--- app/src/main/java/com/example/servicesandwearables/MainActivity.java
+++ app/src/main/java/com/example/servicesandwearables/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.servicesandwearables;
 
+import android.app.Notification;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import createchannel.CreateChannel;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CreateChannel channel = new CreateChannel(this);
+        channel.createChannel();
         btnDisplayNotification1 = findViewById(R.id.btnDisplayNotification1);
         getBtnDisplayNotification2 = findViewById(R.id.btnDisplayNotification2);
 
@@ -24,39 +29,41 @@ public class MainActivity extends AppCompatActivity {
         btnDisplayNotification1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                NotificationManager1();
+                DisplayNotification();
             }
         });
 
         getBtnDisplayNotification2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                NotificationManager2();
+                DisplayNotification2();
             }
         });
     }
 
-    private void NotificationManager1() {
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this,"Channel1")
+    private void DisplayNotification() {
+        Notification notification = new NotificationCompat.Builder(this,CreateChannel.CHANNEL_1)
                 .setSmallIcon(R.drawable.ic_message_black_24dp)
                 .setContentTitle("First Message")
                 .setContentText("This is First Message")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setCategory(NotificationCompat.CATEGORY_MESSAGE);
 
-        notificationManagerCompat.notify(1,builder.build());
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .build();
+
+        notificationManagerCompat.notify(1,notification);
 
     }
 
-    private void NotificationManager2() {
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(MainActivity.this,"Channel2")
+    private void DisplayNotification2() {
+        Notification notification = new NotificationCompat.Builder(this,CreateChannel.CHANNEL_2)
                 .setSmallIcon(R.drawable.ic_message_black_24dp)
                 .setContentTitle("Second Message")
                 .setContentText("This is Second Message")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setCategory(NotificationCompat.CATEGORY_MESSAGE);
 
-        notificationManagerCompat.notify(2,builder.build());
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .build();
+
+        notificationManagerCompat.notify(2,notification);
     }
 
 
